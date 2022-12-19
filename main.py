95d4fd967ecb97fc7c27f7481c7df27145ff0d91
Sondre Rogde <sondrerogde@Sondres-MacBook-Pro.local>
2022-12-19 15:35:49 -0500

    small fix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from bysykkel import nearest_station
 import sys
 
-lat = sys.argv[0]
-lon = sys.argv[1]
-type = sys.argv[2]
+lat = float(sys.argv[0])
+lon = float(sys.argv[1])
+type = int(sys.argv[2])
 
 position = (lat, lon)
+
 near = nearest_station(position=position, type=type)
 print(near)
