e82ce78bf6c4e7a1740520a797a9eb141b69290d
Sondre Rogde <sondrerogde@Sondres-MacBook-Pro.local>
2022-12-19 15:47:25 -0500

    fix

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ lat = sys.argv[0]
 lon = sys.argv[1]
 type = sys.argv[2]
 
+print(lat)
+# print(lon)
+print(type)
+print(type(lat))
+
 position = (lat, lon)
 
 near = nearest_station(position=position, type=type)
